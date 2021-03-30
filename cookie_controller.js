266f135c76893e1fd94612ac4776c34080847205
Alucherdi <hjgo127942@gmail.com>
2021-03-30 02:49:18 +0000

    Create cookie_controller.js
    ;)

---
+++ cookie_controller.js
@@ -0,0 +1,30 @@
+class CookieController{
+	static getAll() {
+		var rawCookies = document.cookie.split("; ")
+		var cookies = {}
+		for (var rawCookie of rawCookies) {
+			var cookie = rawCookie.split("=")
+			cookies[cookie[0]] = cookie[1]
+		}
+
+		return cookies
+	}
+
+    static get(key) {
+		return this.getAll()[key]
+	}
+	
+	static set(key, value) {
+		document.cookie = `${key}=${value}`
+	}
+
+    static remove(key) {
+        document.cookie = `${key}= ; expires=${new Date()} ;`
+    }
+
+    static exist(key) {
+        return document.cookie.includes(key)
+    }
+}
+
+export default CookieController
